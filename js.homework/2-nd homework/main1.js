9f8e4f1d3a299d303cadb3e01b564d96edc1094d
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2020-11-01 14:36:55 +0200

    01.11

--- js.homework/2-nd homework/main1.js
+++ js.homework/2-nd homework/main1.js
@@ -1,1 +1,261 @@
-vrgvrvrv
+ // let numbers = [`1`, true,3,5,3]
+ // console.log(numbers)
+
+//array[0]=1;
+//array[1]=true;
+//array[2]=`xxx`;
+//array[3]=`fevvev`;
+//array[4]=55;
+//array[5]=false;
+//array[6]=`V,VF`;
+//array[7]=666;
+//array[8]=`.,.,`;
+//array[9]=true;
+//for(i=0;i<array.length;i++){
+//    console.log(array[i])
+//}
+
+//for (i = 0; i<10;i++){
+    //document.write(`<div>weofledledewpdepdw</div>`)}
+
+//for (i = 0; i<10;i++){
+    //document.write(`<div>weofledledewpdepdw ${i}</div>`
+//let start = 0;
+//while (start < 20){
+  //  document.write(`<h1>cpdcwp</h1>`)
+ //   start++
+//}
+
+
+// let start = 0;
+// while (start < 20){
+//   document.write(`<h1>cpdcwp ${start} </h1>`)
+//   start++
+// }
+
+// let numbers = [1,2,3,4,5,6,7,8,9,0];
+// console.log(numbers)
+
+// let strings = [`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`0`];
+// console.log(strings)
+
+ // let all = [1,`2`,true,3,`4`,false,5,`mix`]
+ // console.log(all)
+
+// ` let arr = [true,1,23,4,`2`,false,4,2,`xXx`,`red`];
+// for ( i=0 ; i<arr.length;i++)
+//     if(typeof arr[i]  === "boolean"){
+//         console.log(arr[i])
+//     }
+
+// let arr = [true,1,23,4,`2`,false,4,2,`xXx`,`red`];
+// for ( i=0 ; i<arr.length;i++)
+//     if(typeof arr[i]  === "number"){
+//         console.log(arr[i])
+//     }
+
+// let arr = [true,1,23,4,`2`,false,4,2,`xXx`,`red`];
+// for ( i=0 ; i<arr.length;i++)
+//     if(typeof arr[i]  === "string"){
+//         console.log(arr[i])
+//     }
+
+//  let arr = [];
+//     console.log(arr)
+// arr[1] = 100
+//  arr[2]  = `xXx`
+//  arr[3] = 23
+//  arr[4] = `qwerty`
+//  arr[5] = 17
+//  arr[6] = false
+//  arr[7] = 66
+//  arr[8] = `true`
+//  arr[9] = true
+//  arr[10] = `typeof`
+
+// for (i=0;i<10;i++){
+//     console.log(i);
+//     document.write(i)
+// }
+
+ // for (i=0;i<100;i++){
+ //     console.log(i);
+ //     document.write(i);
+ // }
+
+ // for (i=0;i<100;i=i+2){
+ //     console.log(i);
+ //     document.write(i);
+ // }
+
+ // for (i=1;i<=100;i=i+2){
+ //      console.log(i);
+ //      document.write(i);
+ //  }
+
+//!!!CLOCK!!!
+
+ // for (let i=0;i<=2;i++){
+ //     for (let j=0;j<60;j++){
+ //         document.write(`${i} : ${j}  <br>`)
+ //         if (i === 2 && j===0 ){break;}
+ //     }
+ // }
+
+
+ // for (let i=0;i<=2;i++){
+ //     for (let j=0;j<60;j++){
+ //         for (let k=0;k<60;k++){
+ //             document.write(`${i}:${j}:${k} <br>`);
+ //             console.log(`${i}:${j}:${k} <br>`);
+ //             if (i === 2 && j === 20 && k === 0){break;}
+ //         }
+ //         if (i === 2 && j === 20){break;}
+ //     }
+ // }
+
+
+
+//зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та:
+ // 1. перебрати його циклом while
+
+// let arr = [2,17,13,6,22,31,45,66,100,-18]
+// let i =0
+//  while (i<arr.length){
+//     console.log(arr[i])
+//      i++
+//  }
+
+ //2. перебрати його циклом for
+
+ // let arr = [2,17,13,6,22,31,45,66,100,-18]
+ // for (let i=0;i<arr.length;i++){
+ //     console.log(arr[i])
+ // }
+
+
+ //3. перебрати циклом while та вивести  числа тільки з непарним/парним індексом
+
+  // let arr = [2,17,13,6,22,31,45,66,100,-18]
+  //  let i =1
+  //   while (i<arr.length){
+  //      console.log(arr[i])
+  //       i=i+2
+  //   }
+
+  // let arr = [2,17,13,6,22,31,45,66,100,-18]
+  //   let i =0
+  //    while (i<arr.length){
+  //       console.log(arr[i])
+  //        i=i+2
+  //    }
+
+
+ //4.перебрати циклом for та вивести  числа тільки з непарним/парним індексом
+
+  // let arr = [2,17,13,6,22,31,45,66,100,-18]
+  //  for (let i=1;i<arr.length;i=i+2){
+  //      console.log(arr[i])
+  //  }
+
+ // let arr = [2,17,13,6,22,31,45,66,100,-18]
+ // for (let i=0;i<arr.length;i=i+2){
+ //     console.log(arr[i])
+ // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
