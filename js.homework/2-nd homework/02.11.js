b76777d558c3b712eea386c5fdcb1b228220affa
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2020-11-04 17:03:55 +0200

    01.11

---
+++ js.homework/2-nd homework/02.11.js
@@ -0,0 +1,55 @@
+// let arr = ['a', 'b', 'c']
+// arr.push(1,2,3)
+// console.log(arr);
+// let arr = ['js', 'css', 'jq']
+// arr.shift()
+// console.log(arr);
+
+
+// let arr = []
+//
+// for (let i=0;i<=100;i=i+2) {
+//     arr[i] = i
+//     //console.log(arr[i])
+// }
+//
+// for (let j=1;j<100;j=j+2) {
+//     arr[j] = j
+//     //console.log(arr[j])
+// }
+// console.log(arr)
+
+//
+// let arr = [];
+// for ( let i=0; i<100;i++ ){
+//     arr[i] = parseInt(Math.random()*(732 - 8)+8)
+//     if (i%3 === 0){
+//         if (arr[i]%2 === 0){
+//             let arr1 = arr[i];
+//             console.log(arr1);
+//         }
+//
+//     }
+// }
+
+
+
+// let arr = [];
+// for ( let i=0; i<100;i++ ){
+//     arr[i] = parseInt(Math.random()*(732 - 8)+8);
+//     document.write(arr[i] + `_`)
+//     if (arr[i]%2 === 0){
+//         console.log(arr[i-1]);
+//
+//     }
+// }
+
+//
+// let arr = [100,250,50,168,120,345,188];
+// let sum=0;
+// for(let i=0;i<arr.length;i++){
+//     sum = (  sum + (arr[i])/arr.length   );
+//
+// }
+// console.log((sum));
+
