9d07a739540dddc838ca10b180e10143270f6756
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2020-11-25 12:57:11 +0200

    callback hell ,promise, async await

--- js.homework/9-th homework/main.js
+++ js.homework/9-th homework/main.js
@@ -113,111 +113,226 @@
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!Promise!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
 
+// function Myday1(wokeup) {
+//     return new Promise((resolve,reject)=>{
+//     console.log(`Start the day`)
+//     setTimeout(()=>{
+//         if (wokeup) resolve(` Я Прокинуwся`)
+//         else reject(`Сумно, не прокинувся`)
+//     },500)
+// })}
+// ////////////
+// function morning1(eat){
+//     return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log(`Йду снідати`)},279)
+//     setTimeout(()=>{
+//         if (!eat) {
+//             reject(`Нема що кушац `)
+//         }else{
+//             resolve(`Є що кушац`)
+//         }
+//     },750)
+// })}
+// ///////
+// function clean_teeth1(Toothpaste){
+//     return new Promise((resolve, reject) => {
+//     setTimeout(()=>{
+//         console.log('Йду в ванну')},314)
+//
+//     setTimeout(()=>{
+//         if(!Toothpaste) reject(`О ні,закінчилась`)
+//         else{resolve(`Чищу зубки щоб були здорові`)}
+//     },1000)
+//     })}
+// //////
+// function get_dresserd1(clothes){
+//     return new Promise((resolve,reject)=>{
+//
+//         setTimeout(()=>{
+//         console.log(`Йду вдягатись`)
+//     },299)
+//     setTimeout(()=>{
+//         if (!clothes) reject(`Немає одягу - повернусь в ліжко`)
+//         else{resolve(`Вдягаюсь`)
+//         }
+//     },444)})}
+// ////////
+// function goingtostup1(maska){
+//     return new Promise((resolve,reject)=>{
+//
+//         setTimeout(()=>{
+//         console.log(`Йду на зупинку`)
+//     },155)
+//     setTimeout(()=> {
+//         if (!maska) reject(`Ляяя,знову забув маску - повертаюсь`)
+//         else {
+//             resolve(`Чекаю маршрутку`)
+//         }
+//     },345)
+// })}
+// ///
+// function bus1(seat){
+//     return new Promise((resolve,reject)=>{
+//
+//         setTimeout(()=>{
+//         console.log(`Приїхала маршрутка`)
+//     },199)
+//     setTimeout(()=>{
+//         if (!seat)reject(`Лише сидячі місця - чекаю наступну`)
+//         else {resolve(`Їду в палітєх`)}
+//     },299)})}
+//
+// function palitex1(karantyn){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//         console.log(`Вже біля палітєха`)
+//     },199)
+//     setTimeout(()=>{
+//         if (!karantyn) reject(`Заходжу в палітєх і вчусь`)
+//         else { resolve(`Зараз ж карантин - повертаюсь`)}
+//     },299)})}
+//
+// Myday1(1).then(value => {
+//     console.log(value)
+//     return morning1(1)
+// }).then(value => {
+//     console.log(value)
+//     return clean_teeth1(1)
+// }).then(value => {
+//     console.log(value)
+//     return get_dresserd1(1)
+// }).then(value => {
+//     console.log(value)
+//     return goingtostup1(1)
+// }).then(value => {
+//     console.log(value)
+//     return bus1(1)
+// }).then(value => {
+//     console.log(value)
+//     return palitex1(1)
+// }).then(value => {
+//     console.log(value);
+// }).catch(reason => {
+//     console.log(reason)
+// })
+
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!асинк авейт!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+
+
 function Myday1(wokeup) {
     return new Promise((resolve,reject)=>{
-    console.log(`Start the day`)
-    setTimeout(()=>{
-        if (wokeup) resolve(` Я Прокинуwся`)
-        else reject(`Сумно, не прокинувся`)
-    },500)
-})}
-////////////
+        console.log(`Start the day`)
+        setTimeout(()=>{
+            if (wokeup) resolve(` Я Прокинуwся`)
+            else reject(`Сумно, не прокинувся`)
+        },500)
+    })}
 function morning1(eat){
     return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        console.log(`Йду снідати`)},279)
-    setTimeout(()=>{
-        if (!eat) {
-            reject(`Нема що кушац `)
-        }else{
-            resolve(`Є що кушац`)
-        }
-    },750)
-})}
-///////
+        setTimeout(()=>{
+            console.log(`Йду снідати`)},279)
+        setTimeout(()=>{
+            if (!eat) {
+                reject(`Нема що кушац `)
+            }else{
+                resolve(`Є що кушац`)
+            }
+        },750)
+    })}
 function clean_teeth1(Toothpaste){
     return new Promise((resolve, reject) => {
-    setTimeout(()=>{
-        console.log('Йду в ванну')},314)
-
-    setTimeout(()=>{
-        if(!Toothpaste) reject(`О ні,закінчилась`)
-        else{resolve(`Чищу зубки щоб були здорові`)}
-    },1000)
+        setTimeout(()=>{
+            console.log('Йду в ванну')},314)
+        setTimeout(()=>{
+            if(!Toothpaste) reject(`О ні,закінчилась`)
+            else{resolve(`Чищу зубки щоб були здорові`)}
+        },1000)
     })}
-//////
 function get_dresserd1(clothes){
     return new Promise((resolve,reject)=>{
-
         setTimeout(()=>{
-        console.log(`Йду вдягатись`)
-    },299)
-    setTimeout(()=>{
-        if (!clothes) reject(`Немає одягу - повернусь в ліжко`)
-        else{resolve(`Вдягаюсь`)
-        }
-    },444)})}
-////////
+            console.log(`Йду вдягатись`)
+        },299)
+        setTimeout(()=>{
+            if (!clothes) reject(`Немає одягу - повернусь в ліжко`)
+            else{resolve(`Вдягаюсь`)
+            }
+        },444)})}
 function goingtostup1(maska){
     return new Promise((resolve,reject)=>{
-
         setTimeout(()=>{
-        console.log(`Йду на зупинку`)
-    },155)
-    setTimeout(()=> {
-        if (!maska) reject(`Ляяя,знову забув маску - повертаюсь`)
-        else {
-            resolve(`Чекаю маршрутку`)
-        }
-    },345)
-})}
-///
+            console.log(`Йду на зупинку`)
+        },155)
+        setTimeout(()=> {
+            if (!maska) reject(`Ляяя,знову забув маску - повертаюсь`)
+            else {
+                resolve(`Чекаю маршрутку`)
+            }
+        },345)
+    })}
 function bus1(seat){
     return new Promise((resolve,reject)=>{
-
         setTimeout(()=>{
-        console.log(`Приїхала маршрутка`)
-    },199)
-    setTimeout(()=>{
-        if (!seat)reject(`Лише сидячі місця - чекаю наступну`)
-        else {resolve(`Їду в палітєх`)}
-    },299)})}
-
+            console.log(`Приїхала маршрутка`)
+        },199)
+        setTimeout(()=>{
+            if (!seat)reject(`Лише сидячі місця - чекаю наступну`)
+            else {resolve(`Їду в палітєх`)}
+        },299)})}
 function palitex1(karantyn){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
-        console.log(`Вже біля палітєха`)
-    },199)
-    setTimeout(()=>{
-        if (!karantyn) reject(`Заходжу в палітєх і вчусь`)
-        else { resolve(`Зараз ж карантин - повертаюсь`)}
-    },299)})}
-
-Myday1(1).then(value => {
-    console.log(value)
-    return morning1(1)
-}).then(value => {
-    console.log(value)
-    return clean_teeth1(1)
-}).then(value => {
-    console.log(value)
-    return get_dresserd1(1)
-}).then(value => {
-    console.log(value)
-    return goingtostup1(1)
-}).then(value => {
-    console.log(value)
-    return bus1(1)
-}).then(value => {
-    console.log(value)
-    return palitex1(1)
-}).then(value => {
-    console.log(value);
-}).catch(reason => {
-    console.log(reason)
-})
+            console.log(`Вже біля палітєха`)
+        },199)
+        setTimeout(()=>{
+            if (!karantyn) reject(`Заходжу в палітєх і вчусь`)
+            else { resolve(`Зараз ж карантин - повертаюсь`)}
+        },299)})}
+async function sdad(){
+    const wokeup = await Myday1(1)
+    console.log(wokeup)
+    const eat = await morning1(1)
+    console.log(eat)
+    const Toothpaste = await clean_teeth1(1)
+    console.log(Toothpaste)
+    const clothes = await get_dresserd1(1)
+    console.log(clothes)
+    const maska = await goingtostup1(1)
+    console.log(maska)
+    const seat = await  bus1(1)
+    console.log(seat)
+    const karantyn = await palitex1(1)
+    console.log(karantyn)
 
 
-//
+}
+sdad()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
